46db025711b99563b62059dfddf8196e238ceebe
Ryan840627 <89112574+Ryan840627@users.noreply.github.com>
2021-09-27 10:21:09 +0800

    認識靜態API屬性及方法

---
+++ Unity_LionGameDesigh_3DRPG/Assets/Scripts/APIStatic.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+
+/// <summary>
+/// 認識API : 靜態Static
+/// </summary>
+public class APIStatic : MonoBehaviour
+{
+    private void Start()
+    {
+        #region 靜態屬性
+        //取得
+        //語法
+        //類別名稱，靜態屬性
+        float r = Random.value;
+        print("取得靜態屬性隨機值，隨機值 : " + r);
+
+        //設定Set
+        //語法:
+        //類別名稱，靜態屬性 指定 值
+        // 只要看到 Read Only 就不能設定
+        Cursor.visible = false;
+        // Random.value = 99.9f; 唯獨屬性不能設定
+        #endregion
+
+        #region 靜態方法
+        // 呼叫、參數、傳回值
+        // 簽章 : 參數、傳回值
+        //語法:
+        //類別名稱，靜態方法(對應引數)
+        float range =  Random.Range(10.5f, 20.9f);
+        print("隨機範圍 10.5 ~ 20.9 : " + range);
+
+        //API 說明很重要 : 使用整數時不包含最大值
+        int rangeint = Random.Range(1, 3);
+        print("隨機範圍 1 ~ 3 : " + rangeint);
+        #endregion
+    }
+
+    private void Update()
+    {
+        #region 靜態屬性
+        // print("經過多久 :" + Time.timeSinceLevelLoad);
+
+        #endregion
+    }
+}
